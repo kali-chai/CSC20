1719265768d170291e168645b8b1fbdbc69f8e4e
kali-chai <147569507+kali-chai@users.noreply.github.com>
2025-02-17 13:20:16 -0800

    deleted javadocs (broken)

--- README.md
+++
@@ -1,2 +0,0 @@
-# CSC20
-Kali's 2025 Spring CSC 20 Projects. Lots of homework.
--- member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"PackageWilson","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"PackageWilson","l":"PackageWilson()","u":"%3Cinit%3E()"}];updateSearchResults();
