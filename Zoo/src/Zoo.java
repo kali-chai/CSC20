6d4f90aaf373f87850ac6f5449019cb5e8657e77
kali-chai <147569507+kali-chai@users.noreply.github.com>
2025-02-17 13:55:21 -0800

    added a thing

--- Zoo/src/Zoo.java
+++ Zoo/src/Zoo.java
@@ -3,3 +3,39 @@ public class Zoo {
         System.out.println("Hello, World!");
     }
 }
+
+class Animal {
+    private int age;
+    private String name;
+    private String color;
+
+    public Animal(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
